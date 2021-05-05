d2be5e69dfc5aa872357287609984a897407401f
Dinar <47077484+dinarhasanshin@users.noreply.github.com>
2021-05-05 22:12:32 +0400

    initial

--- client/src/components/TodoPage/TodoItem/TodoItem.jsx
+++ client/src/components/TodoPage/TodoItem/TodoItem.jsx
@@ -7,7 +7,8 @@ export const TodoItem = ({item, deleteItem, isCompleted, updateTodo}) => {
         <>
             <div className={s.add_todo + " " + s.todo_items_item}>
                 <div className={s.todo_left}>
-                    <div className={isCompleted === true ? s.check_todo + " " + s.active : s.check_todo} onClick={() => {updateTodo(item._id, "isCompleted", !isCompleted)}}></div>
+                    <button className={isCompleted === true ? s.check_todo + " " + s.active : s.check_todo} onClick={() => {updateTodo(item._id, "isCompleted", !isCompleted)}}>
+                    </button>
                     <div className={s.text_todo}>
                         <span>{item.title}</span>
                     </div>
