c82a0da18fbe68cec6cf133e0fad206c21507dd5
Dinar <47077484+dinarhasanshin@users.noreply.github.com>
2021-05-10 20:09:23 +0400

    Объединение аккаунтов

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const schema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    links: [{type: Types.ObjectId, ref: 'Link'}],
     todos: {type: Types.ObjectId, ref: 'Todo'},
     maxOrder: {type: Number, default: 0, required: true}
 })
